973457f702e11f8fffc6d27d41bec30067470359
Ignacy Berent <2gb02ignac@gmail.com>
2025-05-05 17:20:48 +0200

    fixing esling config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { RouterProvider } from "react-router-dom";
-import { router } from "./lib/routes";
+import { router } from "@/lib/routes";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { queryClient } from "./lib/react-query";
+import { queryClient } from "@/lib/react-query";
 import { I18nextProvider } from "react-i18next";
-import i18n from "./lib/i18n";
+import i18n from "@/lib/i18n";
 
 const App = () => {
   return (
--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import translationEN from "../locales/en.json";
-import translationPL from "../locales/pl.json";
+import translationEN from "@/locales/en.json";
+import translationPL from "@/locales/pl.json";
 
 const resources = {
   en: {
